130c5efafe24ca1ba4abf40f7a2c02549011eca0
hamidrezazargham <hamidrezazargham@gmail.com>
2024-04-30 20:07:37 +0000

    Create top_genre view

--- Views.sql
+++ Views.sql
@@ -1,4 +1,4 @@
-DROP VIEW IF EXISTS low_books, borrowers;
+DROP VIEW IF EXISTS low_books, borrowers, top_genre;
 
 
 -- a view to get books with available copies lower than 5.
@@ -15,3 +15,14 @@ SELECT Title, Name, Email, Borrow_Date, Return_Date
 FROM Books
 JOIN Borrowed_Books ON Borrowed_Books.Book_Id=Books.Book_id
 JOIN Members ON Members.Member_id=Borrowed_Books.Member_id;
+
+
+-- Find the most popular genre in the library.
+-- Display the genre with the highest total number of books borrowed.
+CREATE VIEW top_genre AS
+SELECT Title, Genre, COUNT(*) AS Total_Borrow
+FROM Books
+JOIN Borrowed_Books ON Borrowed_Books.Book_Id=Books.Book_id
+GROUP BY Genre, Title
+ORDER BY total_borrow DESC
+LIMIT 1;
